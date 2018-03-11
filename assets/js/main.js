2d400f4226dbf184db24fecd7e911e79744bb73d
Ricky Hsu <rickyhsu0101@gmail.com>
2018-03-11 14:16:25 -0700

    Replace jQuery for Handlebars to dynamically generate modals and cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,92 +40,70 @@ $(document).ready(function(){
         }
     });
     function createGifCard(rate, still, animate){
-        var card = $("<div>");
-        card.addClass("card gifCard");
-
-        var cardImage = $("<img>");
-        cardImage.addClass("card-img-top");
-        cardImage.attr("src", still);
-        cardImage.attr("data-still", still);
-        cardImage.attr("data-animate", animate);
-
-        var cardFooter = $("<div>");
-        cardFooter.addClass("card-footer");
-        cardFooter.text("Rating: " + rate);
-
-        card.append(cardImage);
-        card.append(cardFooter);
-
+        //use handlebards as templating engine
+        var source = $("#card-template").html();
+        var template = Handlebars.compile(source);
+        var context = {
+            animateSrc: animate,
+            stillSrc: still,
+            ratingString: "Rating: " + rate
+        };
+        var html = template(context);
+        var card = $(html);
         $("#gifCards").append(card);
     }
     function createModal(rating, animateSrc, fullSrc){
-        //create surround modal block
-        var modal = $("<div>");
-        modal.addClass("modal fade");
-        modal.attr("data-src", animateSrc);
-        modal.attr("tabindex", "-1");
-        //set the modal to dialog
-        var modalDialog = $("<div>");
-        modalDialog.addClass("modal-dialog modal-dialog-centered modal-lg");
-        //set modal content block
-        var modalContent = $("<div>");
-        modalContent.addClass("modal-content");
-        //set modal header
-        var modalHeader = $("<div>");
-        modalHeader.addClass("modal-header");
-        //initialize the close icon as a button that dismisses the modal
-        var closeButton = $("<button type='button' class='close' data-dismiss='modal'></button>");
-        var closeIcon = $("<span>&times;</span>");
-        closeButton.append(closeIcon);
-        //append the button to the header
-        modalHeader.append(closeButton);
-        //set modal body
-        var modalBody = $("<div>");
-        modalBody.addClass("modal-body");
-        modalBody.append("<img src = '' data-original = '"+fullSrc+"'>");
-        //set modal footer
-        var modalFooter = $("<div>");
-        modalFooter.addClass("modal-footer");
-        modalFooter.append("<h2>Rating: "+rating+"</h2>");
-        //append header, body, and footer to content
-        modalContent.append(modalHeader, modalBody, modalFooter);
-        //append content to dialog
-        modalDialog.append(modalContent);
-        //append dialog to modal block
-        modal.append(modalDialog);
-        //add the created modal to the modals block
+        //use handlebars as templating engine
+        var source = $("#modal-template").html();
+        var template = Handlebars.compile(source);
+        var context = {
+            animatedGIF: animateSrc,
+            originalGIF: fullSrc,
+            ratingString: "Rating: " + rating
+        };
+        var html = template(context);
+        var modal = $(html);
         $("#modals").append(modal);
     }
     $(document).on("click", ".gifButton", function(){
         var queryValue = $(this).text();
-        //remove the current disabled button if any
-        $(".gifButton.disabled").removeClass("disabled");
-        //add the disabled class to the clicked button
-        //prevents button spamming
-        $(this).addClass("disabled");
-        //empty the div containing the gifs
-        $("#gifCards").empty();
-        $("#modals").empty();
-        //set up query url with existing template defined above
-        var queryUrl = apiUrl + "&q=" + queryValue;
-        $.ajax({
-            method: "GET",
-            url: queryUrl
-        }).done(function(response){
-            response.data.forEach(function(element){
-                //check if the image is a gif
-                if(element.type == "gif"){
-                    var rating = element.rating;
-                    var stillUrl = element.images.fixed_width_still.url;
-                    var animatedUrl = element.images.fixed_width.url;
-                    var fullUrl = element.images.original.url;
-                    createGifCard(rating, stillUrl, animatedUrl);
-                    createModal(rating, animatedUrl, fullUrl);
-                }
+        //check if button is not disabled
+        //disabled is a bootstrap class so it will still trigger click event
+        if(!$(this).hasClass("disabled")){
+            //remove the current disabled button if any
+            $(".gifButton.disabled").removeClass("disabled");
+            //add the disabled class to the clicked button
+            //prevents button spamming
+            $(this).addClass("disabled");
+            //empty the div containing the gifs
+            $("#gifCards").empty();
+            $("#modals").empty();
+            //set up query url with existing template defined above
+            var queryUrl = apiUrl + "&q=" + queryValue;
+            $.ajax({
+                method: "GET",
+                url: queryUrl
+            }).done(function(response){
+                response.data.forEach(function(element){
+                    //check if the image is a gif
+                    if(element.type == "gif"){
+                        //get rating
+                        var rating = element.rating;
+                        //get still image fixed width for card
+                        var stillUrl = element.images.fixed_width_still.url;
+                        //get animated image fixed width for card
+                        var animatedUrl = element.images.fixed_width.url;
+                        //get the orginal animated image
+                        var fullUrl = element.images.original.url;
+                        createGifCard(rating, stillUrl, animatedUrl);
+                        createModal(rating, animatedUrl, fullUrl);
+                    }
+                });
             });
-        });
+        }   
     });
     $(document).on("mouseenter", ".gifCard", function(){
+        //change image source to animated gif 
         var img = $(this).children("img");
         var animatedUrl = img.attr("data-animate");
         img.attr("src", animatedUrl);
@@ -133,12 +111,15 @@ $(document).ready(function(){
         modalImg.attr("src", modalImg.attr("data-original"));
     });
     $(document).on("mouseleave", ".gifCard", function(){
+        //change image source to still gif
         var img = $(this).children("img");
         var stillUrl = img.attr("data-still");
         img.attr("src", stillUrl);
     });
     $(document).on("click", ".gifCard", function(){
+        //show the modal after a click on the card
         var animatedUrl = $(this).children("img").attr("data-animate");
+        //set slight delay for load
         setTimeout(function(){
             $("[data-src = '"+animatedUrl+"']").modal("show");
         }, 700);
